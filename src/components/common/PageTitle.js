a38124b6298d81ca707496899c20342bab5c6f06
Kenneth Ijama <4natcom@gmail.com>
2020-08-24 18:18:00 +0100

    Logout and Dynamic Page title feature created

--- src/components/common/PageTitle.js
+++ src/components/common/PageTitle.js
@@ -4,10 +4,10 @@ import React from "react";
 import Helmet from "react-helmet";
 
 const TitleComponent = ({ title }) => {
-  var defaultTitle = "⚛️ 10Dpredict.com | Challenge to Win";
+  var defaultTitle = "10Dpredict.com | Challenge to Win";
   return (
     <Helmet>
-      <title>{title ? title : defaultTitle}</title>
+      <title>{title ? `${title} - 10Dpredict.com` : defaultTitle}</title>
     </Helmet>
   );
 };
--- src/pages/Signup.js
+++ src/pages/Signup.js
@@ -1,10 +1,12 @@
 import React from "react";
 import SignupForm from "../components/SignupForm";
 import AppContainer from "../components/AppContainer";
+import { TitleComponent } from "../components/common/PageTitle";
 
 const Signup = () => {
   return (
     <AppContainer>
+      <TitleComponent title={"Signup To Predict | Create account "} />
       <SignupForm />
     </AppContainer>
   );
